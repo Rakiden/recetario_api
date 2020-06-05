3f454f7e1521b2097a8f629511fb18f4f60dca4d
Rakiden <rakiden97@gmail.com>
2020-06-05 10:19:51 +0200

    test

--- api.php
+++ api.php
@@ -8,17 +8,17 @@ header("Content-Type: application/json; charset=UTF-8");
 //include "https://serverapiprueba.herokuapp.com/config/config.php";
 
 define('DB_NAME','UZMZrLAgvJ');
-define('DB_USER','UZMZrLAgvJ');
+define('DB_USER','tumadre');
 define('DB_PASSWORD','sxpAB2clyp');
 define('DB_HOST','https://remotefesfsmysql.com/phpmyadmin/sql.php');
-
 
 $mysqli = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
 
 
 
 $postjson = json_decode(file_get_contents('php://input'),true);
-if($postjson['aksi']=="register"){
+if($postjson['mode']=="register"){
+
     $password = md5($postjson['password']);
     $query = mysqli_query($mysqli, "INSERT INTO user SET
         name = '$postjson[name]',
@@ -28,9 +28,9 @@ if($postjson['aksi']=="register"){
         password = '$password'
     ");
     
-    if($query) $result = json_encode(array('success'=>true));
-    else $result = json_encode(array('success'=>false, 'msg'=>$query));
+  if($query) $result = json_encode(array('success'=>true));
+  else $result = json_encode(array('success'=>false, 'msg'=>$query));
 
-    echo $result;
-  }
+  echo $result;
+}
 ?>
