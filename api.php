268eebcd2987ee8e634ac7a2fa7e74f613306979
Rakiden <rakiden97@gmail.com>
2020-06-05 10:28:19 +0200

    test

--- api.php
+++ api.php
@@ -1,16 +1,15 @@
 <?php
-header('Access-Control-Allow-Origin: https://remotefesfsmysql.com');
+header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 header("Content-Type: application/json; charset=UTF-8");
 
-//include "https://serverapiprueba.herokuapp.com/config/config.php";
 
 define('DB_NAME','UZMZrLAgvJ');
 define('DB_USER','UZMZrLAgvJ');
 define('DB_PASSWORD','sxpAB2clyp');
-define('DB_HOST','https://remotefesfsmysql.com/phpmyadmin/sql.php');
+define('DB_HOST','https://remotefesfsmysql.com');
 
 $mysqli = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
 
