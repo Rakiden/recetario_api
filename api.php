0d0b5c42ad2b3ecea85a2c850deb6297b78c432e
Rakiden <rakiden97@gmail.com>
2020-06-05 10:00:55 +0200

    test

--- api.php
+++ api.php
@@ -2,7 +2,7 @@
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, Origin, X-Auth-Token");
 header("Content-Type: application/json; charset=UTF-8");
 
 //include "https://serverapiprueba.herokuapp.com/config/config.php";
